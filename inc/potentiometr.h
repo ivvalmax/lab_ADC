1f3d8766c6e1ccb5f9a416ab1b2c82fd50185517
Валерий Иванов <valeraivanov2681@gmail.com>
2026-02-02 02:39:39 +0300

    project structure corrected

--- inc/potentiometr.h
+++
@@ -1,6 +0,0 @@
-#ifndef POTENTIOMETR_H_
-#define POTENTIOMETR_H_
-
-#include "common.h"
-
-#endif
--- src/adc.c
+++ src/adc.c
@@ -7,7 +7,7 @@ uint16_t value = 0;
 void adcInit(void)
 {
   ADMUX = (1 << REFS0)|(1 << MUX0)|(1 << MUX1);
-  ADCSRA = (1 << ADIE)|(1 << ADFR)|(1<< ADPS2)|(1 << ADEN);
+  ADCSRA = (1 << ADIE)|(1 << ADFR)|(1 << ADPS2)|(1 << ADEN)|(1 << ADPS1)|(1 << ADPS2);
 }
 
 void adcStart(void)
@@ -20,5 +20,10 @@ ISR (ADC_vect)
   value = (ADCH << 8)|ADCL;
   led.g = 1023 - value;
   led.b = 1023 - value;
+
+  OCR1AL = led.b;
+  OCR3BL = led.g;
+  OCR3CL = led.r;
+
   ADCSRA &= ~(1 << ADIF);
 }
