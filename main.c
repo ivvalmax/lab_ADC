26d20a74250f17910b8159d4d366b8255ff6acd8
Валерий Иванов <valeraivanov2681@gmail.com>
2026-02-02 02:58:45 +0300

    the lab 3 end

--- main.c
+++ main.c
@@ -1,1 +1,29 @@
 #include "inc/common.h"
+#include "inc/adc.h"
+#include "inc/PWM.h"
+#include "inc/rgbLed.h"
+
+extern RGBLed_t led;
+
+void allInit(void);
+
+int main(void)
+{
+  allInit();
+  adcStart();
+
+  while(1)
+  {
+    
+  }
+
+}
+
+void allInit(void)
+{
+  adcInit();
+  PWMInit();
+  rgbLedInit();
+
+  sei();
+}
